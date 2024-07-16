ee2c6ae13d27d37d60e23779751f6ce7fc2203b4
Loris Cro <kappaloris@gmail.com>
2024-07-16 19:18:56 +0200

    fix same file in multiple modules error

--- build.zig
+++ build.zig
@@ -5,18 +5,18 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const scripty = b.addModule("scripty", .{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/root.zig"),
     });
+    _ = scripty;
 
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     // unit_tests.strip = true;
 
-    unit_tests.root_module.addImport("scripty", scripty);
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
